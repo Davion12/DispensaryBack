6e2f7fbd1018a7f77528f948ee6c5ec8214a6416
yangzongjun <zongjun.yang@longmaster.com.cn>
2018-05-08 16:21:25 +0800

    第一次演示，还存在绑定元素刷新问题

--- Public/js/Main/main.js
+++ Public/js/Main/main.js
@@ -31,7 +31,7 @@ $(function () {
     });
 
     $('#window_page').click(function () {
-       $("#changePage").load("./changeWindowPage");
+        $("#changePage").load("./changeWindowPage");
         transferNego("#window_page");
         window.onload = displayWindowInfo();
     });
@@ -49,8 +49,8 @@ $(function () {
 
     $('#dispensary_page').click(function () {
         $("#changePage").load("./changeDispensaryPage");
-       transferNego("#dispensary_page");
-       window.onload = displayDispensaryInfo();
+        transferNego("#dispensary_page");
+        window.onload = displayDispensaryInfo(1);
     });
 });
 
@@ -58,7 +58,7 @@ $(function () {
 function chooseDispensary() {
     $("#changePage").load("./changeDispensaryPage");
     transferNego("#dispensary_page");
-    window.onload = displayDispensaryInfo();
+    window.onload = displayDispensaryInfo(1);
 }
 
 function chooseWindow() {
@@ -80,52 +80,57 @@ var accountInfo;
 
 
 function transferNego(id) {
-    $("#window_page").attr("class","noactive");
-    $("#main_page").attr("class","noactive");
-    $("#account_page").attr("class","noactive");
-    $("#dispensary_page").attr("class","noactive");
-    $(id).attr("class","active");
+    $("#window_page").attr("class", "noactive");
+    $("#main_page").attr("class", "noactive");
+    $("#account_page").attr("class", "noactive");
+    $("#dispensary_page").attr("class", "noactive");
+    $(id).attr("class", "active");
 }
 
 function displayFrame(type, id) {
     $('.theme-popover-mask').fadeIn(100);
     $('.theme-popover').slideDown(200);
-    if(type === 1){
+    $("textarea").html("");
+    if(id)
+    {
+        if (type === 1) {
             var index = findIndex(dispensaryInfo, id);
             $('#dispensary_id').html(dispensaryInfo[index]["room_id"]);
             $('#dispensary_name').html(dispensaryInfo[index]["room_name"]);
             $('#rule').html(dispensaryInfo[index]["rule"]);
-    } else if(type === 2){
-        var index = findIndex(windowInfo, id);
-        $('#dispensary_id').html(windowInfo[index]["window_id"]);
-        $('#dispensary_name').html(windowInfo[index]["window_name"]);
-        $('#rule').html(windowInfo[index]["room_id"]);
-    } else {
-        var index = findIndex(accountInfo, id);
-        $('#dispensary_id').html(accountInfo[index]["account_id"]);
-        $('#dispensary_name').html(accountInfo[index]["account_name"]);
-        $('#rule').html(accountInfo[index]["display_name"]);
-        $('#pwd').html(accountInfo[index]["password"]);
-    }
-    if(id !== undefined){
+        } else if (type === 2) {
+            var index = findIndex(windowInfo, id);
+            $('#dispensary_id').html(windowInfo[index]["window_id"]);
+            $('#dispensary_name').html(windowInfo[index]["window_name"]);
+            $('#rule').html(windowInfo[index]["room_id"]);
+        } else {
+            var index = findIndex(accountInfo, id);
+            $('#dispensary_id').html(accountInfo[index]["account_id"]);
+            $('#dispensary_name').html(accountInfo[index]["account_name"]);
+            $('#rule').html(accountInfo[index]["display_name"]);
+            $('#pwd').html(accountInfo[index]["password"]);
+        }
         $('#dispensary_id').attr("readonly", "readonly");
+    } else {
+        $('#dispensary_id').removeAttr("readonly");
     }
+
 }
 
-function deleteContent(type, id){
-    if(type == 1){
+function deleteContent(type, id) {
+    if (type == 1) {
         var index = findIndex(dispensaryInfo, id);
-        if(index !== -1){
+        if (index !== -1) {
             //药房删除
             $.ajax({
                 url: "../Page/deleteDispensaryInfo",
                 type: "post",
-                data: {'room_id' : dispensaryInfo[index]["room_id"]},
+                data: {'room_id': dispensaryInfo[index]["room_id"]},
                 dataType: 'json',
                 success: function (jsonData) {
-                    if(jsonData === 1){
+                    if (jsonData === 1) {
                         alert("删除成功");
-                        displayDispensaryInfo();
+                        displayDispensaryInfo(0);
                     } else {
                         alert("删除失败");
                     }
@@ -134,18 +139,18 @@ function deleteContent(type, id){
         } else {
             alert("请选中");
         }
-    } else if(type == 2){
+    } else if (type == 2) {
         //窗口删除
         var index = findIndex(windowInfo, id);
-        if(index !== -1){
+        if (index !== -1) {
             //药房删除
             $.ajax({
                 url: "../Page/deleteWindowInfo",
                 type: "post",
-                data: {'window_id' : windowInfo[index]["window_id"]},
+                data: {'window_id': windowInfo[index]["window_id"]},
                 dataType: 'json',
                 success: function (jsonData) {
-                    if(jsonData === 1){
+                    if (jsonData === 1) {
                         alert("删除成功");
                         displayWindowInfo();
                     } else {
@@ -160,17 +165,17 @@ function deleteContent(type, id){
         //账号删除
         //窗口删除
         var index = findIndex(accountInfo, id);
-        if(index !== -1){
+        if (index !== -1) {
             //药房删除
             $.ajax({
                 url: "../Page/deleteAccountInfo",
                 type: "post",
-                data: {'account_id' : accountInfo[index]["account_id"]},
+                data: {'account_id': accountInfo[index]["account_id"]},
                 dataType: 'json',
                 success: function (jsonData) {
-                    if(jsonData === 1){
+                    if (jsonData === 1) {
                         alert("删除成功");
-                        displayWindowInfo();
+                        displayAccountInfo();
                     } else {
                         alert("删除失败");
                     }
@@ -182,39 +187,26 @@ function deleteContent(type, id){
     }
 }
 
-function hideFrame(type) {
+function hideFrame() {
     $('.theme-popover-mask').fadeOut(100);
     $('.theme-popover').slideUp(200);
-    if((type === 1) || (type === 2)){
-        $('#dispensary_id').html(dispensaryInfo[0]["room_id"]);
-        $('#dispensary_name').html(dispensaryInfo[0]["room_name"]);
-        $('#rule').html(dispensaryInfo[0]["rule"]);
-
-    } else {
-        $('#dispensary_id').html("");
-        $('#dispensary_name').html("");
-        $('#rule').html("");
-        $('#pwd').html("");
-    }
 }
 
-
-function displayDispensaryInfo() {
+function displayDispensaryInfo(type) {
     $.ajax({
         url: "../Page/getDispensaryInfo",
         type: "GET",
         success: function (jsonData) {
-                dispensaryInfo = jsonData;
-                for(var i = 0; i < jsonData.length; i++)
-                {
-                    var trHtml = "<tr>";
-                    trHtml += "<td>" + jsonData[i]["room_id"] + "</td>";
-                    trHtml += "<td>" + jsonData[i]["room_name"] + "</td>";
-                    trHtml += "<td>" + jsonData[i]["rule"] + "</td>";
-                    trHtml += "<td> <a href='javascript:' onclick='displayFrame(1, "+jsonData[i]['room_id']+")' id='modify'>修改</a>  |  <a href='' onclick='deleteContent(1, "+jsonData[i]['room_id']+")'>删除</a></td>";
-                    $("#dispensary_table").append(trHtml);
-                }
-
+            dispensaryInfo = jsonData;
+            $('#dispensary_table').find("td").remove();
+            for (var i = 0; i < jsonData.length; i++) {
+                var trHtml = "<tr>";
+                trHtml += "<td>" + jsonData[i]["room_id"] + "</td>";
+                trHtml += "<td>" + jsonData[i]["room_name"] + "</td>";
+                trHtml += "<td>" + jsonData[i]["rule"] + "</td>";
+                trHtml += "<td> <a href='javascript:' onclick='displayFrame(1, " + jsonData[i]['room_id'] + ")' id='modify'>修改</a>  |  <a href='javascript:void(0)' onclick='deleteContent(1, " + jsonData[i]['room_id'] + ")'>删除</a></td>";
+                $("#dispensary_table").append(trHtml);
+            }
         }
     })
 }
@@ -225,16 +217,16 @@ function displayWindowInfo() {
         url: "../Page/getWindowInfo",
         type: "GET",
         success: function (jsonData) {
-                windowInfo = jsonData;
-                for(var i = 0; i < jsonData.length; i++)
-                {
-                    var trHtml = "<tr>";
-                    trHtml += "<td>" + jsonData[i]["window_id"] + "</td>";
-                    trHtml += "<td>" + jsonData[i]["window_name"] + "</td>";
-                    trHtml += "<td>" + jsonData[i]["room_id"] + "</td>";
-                    trHtml += "<td> <a href='javascript:' onclick='displayFrame(2, "+jsonData[i]['window_id']+")' id='modify'>修改</a>  |  <a href='' onclick='deleteContent(2, "+jsonData[i]['window_id']+")'>删除</a></td>";
-                    $("#dispensary_table").append(trHtml);
-                }
+            windowInfo = jsonData;
+            $('#dispensary_table').find("td").remove();
+            for (var i = 0; i < jsonData.length; i++) {
+                var trHtml = "<tr>";
+                trHtml += "<td>" + jsonData[i]["window_id"] + "</td>";
+                trHtml += "<td>" + jsonData[i]["window_name"] + "</td>";
+                trHtml += "<td>" + jsonData[i]["room_id"] + "</td>";
+                trHtml += "<td> <a href='javascript:' onclick='displayFrame(2, " + jsonData[i]['window_id'] + ")' id='modify'>修改</a>  |  <a href='javascript:void(0)' onclick='deleteContent(2, " + jsonData[i]['window_id'] + ")'>删除</a></td>";
+                $("#dispensary_table").append(trHtml);
+            }
 
         }
     })
@@ -245,25 +237,25 @@ function displayAccountInfo() {
         url: "../Page/getAccountInfo",
         type: "GET",
         success: function (jsonData) {
-                accountInfo = jsonData;
-                for(var i = 0; i < jsonData.length; i++)
-                {
-                    var trHtml = "<tr>";
-                    trHtml += "<td>" + jsonData[i]["account_id"] + "</td>";
-                    trHtml += "<td>" + jsonData[i]["account_name"] + "</td>";
-                    trHtml += "<td>" + jsonData[i]["display_name"] + "</td>";
-                    trHtml += "<td>" + jsonData[i]["password"] + "</td>";
-                    trHtml += "<td> <a href='javascript:' onclick='displayFrame(3, "+jsonData[i]['account_id']+")' id='modify'>修改</a>  |  <a href='' onclick='deleteContent(3, "+jsonData[i]['account_id']+")'>删除</a></td>";
-                    $("#dispensary_table").append(trHtml);
-                }
+            accountInfo = jsonData;
+            $('#dispensary_table').find("td").remove();
+            for (var i = 0; i < jsonData.length; i++) {
+                var trHtml = "<tr>";
+                trHtml += "<td>" + jsonData[i]["account_id"] + "</td>";
+                trHtml += "<td>" + jsonData[i]["account_name"] + "</td>";
+                trHtml += "<td>" + jsonData[i]["display_name"] + "</td>";
+                trHtml += "<td>" + jsonData[i]["password"] + "</td>";
+                trHtml += "<td> <a href='javascript:' onclick='displayFrame(3, " + jsonData[i]['account_id'] + ")' id='modify'>修改</a>  |  <a href='javascript:void(0)' onclick='deleteContent(3, " + jsonData[i]['account_id'] + ")'>删除</a></td>";
+                $("#dispensary_table").append(trHtml);
+            }
         }
     })
 }
 
 function findIndex(source, value) {
-    for(var i= 0; i < source.length; i++){
-        for(var key in source[i]){
-            if(value == source[i][key]){
+    for (var i = 0; i < source.length; i++) {
+        for (var key in source[i]) {
+            if (value == source[i][key]) {
                 return i;
             }
         }
@@ -272,7 +264,7 @@ function findIndex(source, value) {
 }
 
 function submitModify(part) {
-    if(part == 1){
+    if (part == 1) {
         //药房修改
         var params = {
             "room_id": $('#dispensary_id').val(),
@@ -283,17 +275,18 @@ function submitModify(part) {
             url: "../Page/modifyDispensaryInfo",
             type: "POST",
             data: params,
-            dataType:"json",
+            dataType: "json",
             success: function (jsonData) {
-                if(jsonData !== 0){
+                if (jsonData !== 0) {
                     alert("修改成功");
                 } else {
                     alert("修改失败");
                 }
-                displayDispensaryInfo();
+                hideFrame();
+                displayDispensaryInfo(0);
             }
         })
-    } else if(part == 2){
+    } else if (part == 2) {
         //窗口修改
         var params = {
             "window_id": $('#dispensary_id').val(),
@@ -304,13 +297,14 @@ function submitModify(part) {
             url: "../Page/modifyWindowInfo",
             type: "POST",
             data: params,
-            dataType:"json",
+            dataType: "json",
             success: function (jsonData) {
-                if(jsonData !== 1){
+                if (jsonData !== 0) {
                     alert("修改成功");
                 } else {
                     alert("修改失败");
                 }
+                hideFrame();
                 displayWindowInfo();
             }
         })
@@ -325,13 +319,14 @@ function submitModify(part) {
             url: "../Page/modifyAccountInfo",
             type: "POST",
             data: params,
-            dataType:"json",
+            dataType: "json",
             success: function (jsonData) {
-                if(jsonData == 1){
+                if (jsonData !== 0) {
                     alert("修改成功");
                 } else {
                     alert("内容一致，无需修改");
                 }
+                hideFrame();
                 displayAccountInfo();
             }
         })
